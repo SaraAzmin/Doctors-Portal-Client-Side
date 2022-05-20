63f9ff1db28a37bcbc171ccc3621e3068f605d0c
SaraAzmin <azmin.sara17@gmail.com>
2022-05-20 20:25:23 +0200

    basic react route setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About'
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
