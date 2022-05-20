824477e5ec79758b96dee9ce49cd9050d1af8827
SaraAzmin <azmin.sara17@gmail.com>
2022-05-20 20:39:37 +0200

    basic navbar modification and link added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Navbar from './Pages/Shared/Navbar';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './Pages/Home/Home';
-import About from './Pages/About/About'
+import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
