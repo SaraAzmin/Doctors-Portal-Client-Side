cc0345bfc63046934b33dc7e26f850bedfcc88de
SaraAzmin <azmin.sara17@gmail.com>
2022-05-23 00:34:43 +0200

    available slot fixed

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -9,8 +9,10 @@ const AvailableAppointments = ({ date, setDate }) => {
 
     const [treatment, setTreatment] = useState(null);
 
+    const formattedDate = format(date, 'PP');
+
     useEffect(() => {
-        fetch('http://localhost:5000/service')
+        fetch(`http://localhost:5000/available?date=${formattedDate}`)
             .then(res => res.json())
             .then(data => setServices(data))
     }, [])
