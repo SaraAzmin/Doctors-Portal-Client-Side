894c829734d0c5d1f91113b04fc7e96641f12880
SaraAzmin <azmin.sara17@gmail.com>
2022-05-23 23:02:39 +0200

    use token for adding user

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Dashboard from './Pages/Dashboard/Dashboard';
 import MyAppointment from './Pages/Dashboard/MyAppointment';
 import MyReview from './Pages/Dashboard/MyReview';
+import MyHistory from './Pages/Dashboard/MyHistory';
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
         <Route path="dashboard" element={<RequireAuth><Dashboard /></RequireAuth>} >
           <Route index element={<MyAppointment></MyAppointment>}></Route>
           <Route path="review" element={<MyReview></MyReview>}></Route>
+          <Route path="history" element={<MyHistory></MyHistory>}></Route>
 
         </Route>
         <Route path="/register" element={<Register></Register>} />
--- src/Pages/Login/Register.js
+++ src/Pages/Login/Register.js
@@ -3,6 +3,7 @@ import { useCreateUserWithEmailAndPassword, useSignInWithGoogle, useUpdateProfil
 import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
+import useToken from '../../Hooks/useToken';
 import Loading from '../Shared/Loading';
 
 const Register = () => {
@@ -11,14 +12,15 @@ const Register = () => {
     const [createUserWithEmailAndPassword, user, loading, error] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
     const [updateProfile, updating, updateError] = useUpdateProfile(auth);
     const navigate = useNavigate();
-
-
     const { register, formState: { errors }, handleSubmit } = useForm();
+    const [token] = useToken(user || userGoogle);
+
     let registerError;
 
 
     if (user || userGoogle) {
         console.log(user || userGoogle);
+        //navigate('/appointment');
     }
 
     if (loading || loadingGoogle || updating) {
@@ -34,7 +36,7 @@ const Register = () => {
         await createUserWithEmailAndPassword(data.email, data.password);
         await updateProfile({ displayName: data.name });
         console.log('update done');
-        navigate('/appointment');
+        //navigate('/appointment');
     }
 
     return (
