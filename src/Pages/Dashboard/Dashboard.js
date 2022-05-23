90427f1b8b69ff40a5eb19499973575e8c2b5e49
SaraAzmin <azmin.sara17@gmail.com>
2022-05-23 21:12:25 +0200

    appointments loaded for user

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -6,7 +6,7 @@ const Dashboard = () => {
         <div className="drawer drawer-mobile">
             <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content">
-                <h2 className='text-2xl font-bold text-purple-500'>Welcome to your Dashboard</h2>
+                <h2 className='text-3xl font-semibold text-secondary text-center py-5 uppercase'>Welcome to your Dashboard</h2>
                 <Outlet></Outlet>
             </div>
             <div class="drawer-side">
--- src/Pages/Dashboard/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointment.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyAppointment = () => {
+
+    const [appointments, setAppointments] = useState([]);
+    const [user] = useAuthState(auth);
+
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/booking?patiant=${user.email}`)
+                .then(res => res.json())
+                .then(data => setAppointments(data));
+        }
+    }, [user])
+
     return (
         <div>
+            <h2 className='py-5 text-xl text-secondary text-center'>My Appointments: {appointments.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr className='text-center'>
+                            <th>SL. No</th>
+                            <th>Name</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>Treatment</th>
+                        </tr>
+                    </thead>
+                    <tbody className='text-center'>
+                        {
+                            appointments.map((a, index) => <tr>
+                                <th>{index + 1}</th>
+                                <td>{a.patiantName}</td>
+                                <td>{a.date}</td>
+                                <td>{a.slot}</td>
+                                <td>{a.treatment}</td>
+                            </tr>)
+                        }
 
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
