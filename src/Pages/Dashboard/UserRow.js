6a115e31a4bd2fb94fd451bbeea94b40b30231eb
SaraAzmin <azmin.sara17@gmail.com>
2022-05-24 14:12:01 +0200

    admin adding option

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -1,23 +1,29 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
-const UserRow = ({ user }) => {
+const UserRow = ({ user, refetch }) => {
 
-    const { email } = user;
+    const { email, role } = user;
 
     const makeAdmin = () => {
         fetch(`http://localhost:5000/user/admin/${user.email}`, {
-            method: 'GET',
+            method: 'PUT',
             headers: {
                 authorization: `Bearer ${localStorage.getItem('accessToken')}`
             }
         })
+            .then(res => res.json())
+            .then(data => {
+                refetch();
+                toast.success('Admin added');
+            })
     }
 
     return (
         <tr className='text-center'>
             <th>1</th>
-            <td>{user.email}</td>
-            <td><button className="btn btn-xs btn-secondary" onClick={makeAdmin}>Make Admin</button></td>
+            <td>{email}</td>
+            <td>{role == 'admin' ? <span className='font-bold'>ADMIN</span> : <button className="btn btn-xs btn-secondary" onClick={makeAdmin}>Make Admin</button>}</td>
             <td><button className="btn btn-xs btn-primary">Remove User</button></td>
         </tr>
     );
--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -5,7 +5,7 @@ import UserRow from './UserRow';
 
 const Users = () => {
 
-    const { data: users, isLoading } = useQuery('users', () => fetch('http://localhost:5000/user', {
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user', {
         method: 'GET',
         headers: {
             authorization: `Bearer ${localStorage.getItem('accessToken')}`
@@ -29,7 +29,7 @@ const Users = () => {
                 </thead>
                 <tbody>
                     {
-                        users.map(user => <UserRow key={user._id} user={user}></UserRow>)
+                        users.map(user => <UserRow key={user._id} user={user} refetch={refetch}></UserRow>)
                     }
                 </tbody>
             </table>
