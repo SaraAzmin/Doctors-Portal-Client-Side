b51445581904689614cc927e03db47f6195d8a4a
SaraAzmin <azmin.sara17@gmail.com>
2022-05-26 12:31:01 +0200

    payment integrated

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../Shared/Loading';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51L3Q5MChR64wzhkKSbLssex9K6cpixSCzi3xlduqQpyWAYPDHOQoYCm0xjIHnnv5oqlRF7BqPAUCLQ3ls4M2iZzJ00scpTTpVz');
+
 
 const Payment = () => {
 
@@ -17,8 +23,22 @@ const Payment = () => {
 
 
     return (
-        <div>
-            <h1>payment for: {id}</h1>
+        <div className='flex flex-col'>
+            <div class="card w-50 max-w-md bg-base-100 shadow-xl my-5">
+                <div class="card-body">
+                    <p className='font-bold'> Hello <span className='text-success'>{appointment.patiantName}</span>,</p>
+                    <h2 class="card-title">Pay for {appointment.treatment}</h2>
+                    <p>We look forward to meet you on <span className='text-red-600'>{appointment.date}</span> on <span className='text-red-600'>{appointment.slot}</span> </p>
+                    <p>Please pay: ${appointment.price}</p>
+                </div>
+            </div>
+            <div class="card shadow-2xl w-50 max-w-md bg-base-100">
+                <div class="card-body">
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm appointment={appointment} />
+                    </Elements>
+                </div>
+            </div>
         </div>
     );
 };
